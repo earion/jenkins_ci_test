9bf9117db59e340c5cd7fd5431bb5e84952f5c6b
Mateusz Batok <mateusz.batok@orange.com>
2014-09-21 19:39:03 +0200

    Add first Junit Tests

--- src/test/java/pl/jenkins/test/TestBankAccount.java
+++ src/test/java/pl/jenkins/test/TestBankAccount.java
@@ -2,11 +2,20 @@ package pl.jenkins.test;
 
 
 import junit.framework.Assert;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import pl.jenkins.BankAccount;
+
 public class TestBankAccount {
 
-	
+	@Test
+	public void shoudDebetHasSufficentFunds() {
+		BankAccount account = new BankAccount(10);
+		double amount = account.debit(5);
+		Assert.assertEquals(5.0, amount);
+		
+	}
 	
 }
